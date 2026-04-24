7477161d2bd0e2d0aeac0dedada65d63fdd45292
Dabhi Amit <amitdabhi526@gmail.com>
2026-04-24 12:31:53 +0530

    comment out old react code and implement api fetch

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,71 +4,62 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 const App = () => {
-  const initialData = [
-    {
-      id: 1,
-      title: "Reading",
-      description: "I need to read some books",
-      status: "complete",
-    },
-  ];
-
-  const [todoData, setTodoData] = useState(initialData);
+  const [todoData, setTodoData] = useState([]);
 
   const [editValue, setEditValue] = useState(null);
 
-  const add = (input) => {
-    if (!input.title || !input.description) {
-      return alert("Title and Description must be required");
-    }
+  // const add = (input) => {
+  //   if (!input.title || !input.description) {
+  //     return alert("Title and Description must be required");
+  //   }
 
-    if (editValue) {
-      const updated = todoData.map((t) =>
-        t.id === editValue.id
-          ? {
-              ...t,
-              title: input.title,
-              description: input.description,
-              status: "pending",
-            }
-          : t,
-      );
+  //   if (editValue) {
+  //     const updated = todoData.map((t) =>
+  //       t.id === editValue.id
+  //         ? {
+  //             ...t,
+  //             title: input.title,
+  //             description: input.description,
+  //             status: "pending",
+  //           }
+  //         : t,
+  //     );
 
-      setTodoData(updated);
-      setEditValue(null);
-    } else {
-      const newTodo = {
-        id: new Date().getTime(),
-        title: input.title,
-        description: input.description,
-        status: "pending",
-      };
-      setTodoData((prev) => [...prev, newTodo]);
-    }
-  };
+  //     setTodoData(updated);
+  //     setEditValue(null);
+  //   } else {
+  //     const newTodo = {
+  //       id: new Date().getTime(),
+  //       title: input.title,
+  //       description: input.description,
+  //       status: "pending",
+  //     };
+  //     setTodoData((prev) => [...prev, newTodo]);
+  //   }
+  // };
 
-  const edit = (id) => {
-    const selected = todoData.find((t) => t.id === id);
-    setEditValue(selected);
-  };
+  // const edit = (id) => {
+  //   const selected = todoData.find((t) => t.id === id);
+  //   setEditValue(selected);
+  // };
 
-  const deleteTodo = (id) => {
-    const deleteTodo = todoData.filter((t) => t.id !== id);
-    setTodoData(deleteTodo);
-  };
+  // const deleteTodo = (id) => {
+  //   const deleteTodo = todoData.filter((t) => t.id !== id);
+  //   setTodoData(deleteTodo);
+  // };
 
-  const statusEdit = (id) => {
-    const updated = todoData.map((t) => {
-      return t.id === id
-        ? {
-            ...t,
-            status: t.status === "complete" ? "pending" : "complete",
-          }
-        : t;
-    });
+  // const statusEdit = (id) => {
+  //   const updated = todoData.map((t) => {
+  //     return t.id === id
+  //       ? {
+  //           ...t,
+  //           status: t.status === "complete" ? "pending" : "complete",
+  //         }
+  //       : t;
+  //   });
 
-    setTodoData(updated);
-  };
+  //   setTodoData(updated);
+  // };
 
   return (
     <>
